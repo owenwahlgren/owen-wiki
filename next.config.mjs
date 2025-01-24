648ab8f8a2490237cafb8793b4add0fffad94e6c
owen <owenwahlgren@gmail.com>
2025-01-24 11:42:08 -0500

    :pray:

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,10 @@
 const nextConfig = {
     output: "export",
   
-    basePath: "/owen-wiki",
-    assetPrefix: "/owen-wiki",
-  
     images: {
       unoptimized: true,
     },
-  };
+};
   
-  export default nextConfig;
+export default nextConfig;
   
