fb3272f25e195258dd9eccc3e663580a09613faf
owen <owenwahlgren@gmail.com>
2025-01-24 00:42:17 -0500

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /**
-* @type {import('next').NextConfig}
-*/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "/",
-};
-
-export default nextConfig;
+    output: "export",
+  
+    basePath: "/owen-wiki",
+    assetPrefix: "/owen-wiki",
+  
+    images: {
+      unoptimized: true,
+      loader: "akamai",
+      path: "",
+    },
+  };
+  
+  export default nextConfig;
+  
